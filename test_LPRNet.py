5716e3088cfeda9217a987768fbfb7f99b0d0f1e
bernard0047 <avikkuthiala@gmail.com>
2020-11-20 02:47:28 +0530

    added evaluation metrics and char acc

--- test_LPRNet.py
+++ test_LPRNet.py
@@ -6,6 +6,7 @@ test pretrained model.
 '''
 
 from data.load_data import CHARS, CHARS_DICT, LPRDataLoader
+from sklearn.metrics import classification_report
 from PIL import Image, ImageDraw, ImageFont
 from model.LPRNet import build_lprnet
 # import torch.backends.cudnn as cudnn
@@ -14,8 +15,10 @@ import torch.nn.functional as F
 from torch.utils.data import *
 from torch import optim
 import torch.nn as nn
+import pandas as pd
 import numpy as np
 import argparse
+import shutil
 import torch
 import time
 import cv2
@@ -24,15 +27,15 @@ import os
 def get_parser():
     parser = argparse.ArgumentParser(description='parameters to train net')
     parser.add_argument('--img_size', default=(94, 24), help='the image size')
-    parser.add_argument('--test_img_dirs', default="./data/test", help='the test images path')
+    parser.add_argument('--test_img_dirs', default="./images_1/train", help='the test images path')
     parser.add_argument('--dropout_rate', default=0, help='dropout rate.')
     parser.add_argument('--lpr_max_len', default=15, help='license plate number max length.')
-    parser.add_argument('--test_batch_size', default=100, help='testing batch size.')
+    parser.add_argument('--test_batch_size', default=64, help='testing batch size.')
     parser.add_argument('--phase_train', default=False, type=bool, help='train or test phase flag.')
     parser.add_argument('--num_workers', default=8, type=int, help='Number of workers used in dataloading')
     parser.add_argument('--cuda', default=True, type=bool, help='Use cuda to train model')
     parser.add_argument('--show', default=False, type=bool, help='show test image and its predict result or not.')
-    parser.add_argument('--pretrained_model', default='./weights/Final_LPRNet_model.pth', help='pretrained base model')
+    parser.add_argument('--pretrained_model', default='./weights/topmodel.pth', help='pretrained base model')
 
     args = parser.parse_args()
 
@@ -42,14 +45,15 @@ def collate_fn(batch):
     imgs = []
     labels = []
     lengths = []
+    filenames = []
     for _, sample in enumerate(batch):
-        img, label, length = sample
+        img, label, length, filename = sample
         imgs.append(torch.from_numpy(img))
         labels.extend(label)
         lengths.append(length)
     labels = np.asarray(labels).flatten().astype(np.float32)
 
-    return (torch.stack(imgs, 0), torch.from_numpy(labels), lengths)
+    return (torch.stack(imgs, 0), torch.from_numpy(labels), lengths, filenames)
 
 def test():
     args = get_parser()
@@ -78,14 +82,21 @@ def Greedy_Decode_Eval(Net, datasets, args):
     # TestNet = Net.eval()
     epoch_size = len(datasets) // args.test_batch_size
     batch_iterator = iter(DataLoader(datasets, args.test_batch_size, shuffle=True, num_workers=args.num_workers, collate_fn=collate_fn))
+    c_matrix = np.zeros((len(CHARS)-1,len(CHARS)-1))
 
     Tp = 0
     Tn_1 = 0
     Tn_2 = 0
+    t_chars = 0
+    T_c = 0
+    T_f = 0
+    T_fc = 0
+    t_fchars = 0
+    res_chars = np.zeros(len(CHARS))
     t1 = time.time()
     for i in range(epoch_size):
         # load train data
-        images, labels, lengths = next(batch_iterator)
+        images, labels, lengths, filenames = next(batch_iterator)
         start = 0
         targets = []
         for length in lengths:
@@ -122,21 +133,98 @@ def Greedy_Decode_Eval(Net, datasets, args):
                 no_repeat_blank_label.append(c)
                 pre_c = c
             preb_labels.append(no_repeat_blank_label)
+        #print(len(preb_labels))
+    
+
         for i, label in enumerate(preb_labels):
+            X=i
+            # lb = ""
+            # tg = ""
+            # for j in targets[i]:
+            #     x = int(j)
+            #     tg+= CHARS[x]
+            # for j in label:
+            #     lb += CHARS[j]
+
             # show image and its predict label
+            t_chars+=len(targets[i])
+            for j in range(len(label)):
+                if j>=len(targets[i]):
+                    continue
+                if label[j] == targets[i][j]:
+                    res_chars[label[j]]+=1
+                    T_c+=1
             if args.show:
                 show(imgs[i], label, targets[i])
             if len(label) != len(targets[i]):
+                #print(abs(len(label)-len(targets[i])))
                 Tn_1 += 1
-                continue
-            if (np.asarray(targets[i]) == np.asarray(label)).all():
-                Tp += 1
+
             else:
-                Tn_2 += 1
+                c_matrix = cmatrix(c_matrix,label,targets[i])
+                t_fchars+=len(targets[i])
+                for j in range(len(label)):
+                    if j>=len(targets[i]):
+                        continue
+                    if label[j] == targets[i][j]:
+                        res_chars[label[j]]+=1
+                        T_fc+=1
+                fuzzy = 0
+                for x in range(len(label)):
+                    if targets[i][x]==label[x]:
+                        fuzzy += 1
+                if fuzzy/len(label) >= 0.75:
+                    T_f += 1
+                if (np.asarray(targets[i]) == np.asarray(label)).all():
+                    #print(label,"<--->",targets[i])
+                    Tp += 1
+                else:
+                    Tn_2 += 1
+            # print(lb,tg)
+            # if not os.path.isdir('./0labelpreds'):
+            #     os.makedirs('./0labelpreds')
+            # rename = '0labelpreds/'+filenames[X].split('\\')[1].split('.')[0]+"__"+lb+'.png'
+            # shutil.copy(filenames[X],rename)
+    evaluate_and_save(c_matrix)
+
+            
     Acc = Tp * 1.0 / (Tp + Tn_1 + Tn_2)
     print("[Info] Test Accuracy: {} [{}:{}:{}:{}]".format(Acc, Tp, Tn_1, Tn_2, (Tp+Tn_1+Tn_2)))
+    print(f"[Info] 75%+ Accuracy: {T_f/(Tp+Tn_1+Tn_2)} [{T_f}/{(Tp+Tn_1+Tn_2)}]")
     t2 = time.time()
+    print(f'[Info] Global Char Accuracy:{T_c/t_chars} [{T_c}/{t_chars}] ')
+    print(f'[Info] Char Accuracy on full length match:{T_fc/t_fchars} [{T_fc}/{t_fchars}] ')
+    # print('Per char: ')
+    # for i in range(10):
+    #     print(i,": ",res_chars[i]/T_c)
+    # for i in range(10,len(CHARS)-1):
+    #     print(chr(55+i),': ',res_chars[i]/T_c)
     print("[Info] Test Speed: {}s 1/{}]".format((t2 - t1) / len(datasets), len(datasets)))
+
+
+def cmatrix(matrix,label,target):
+    for i in range(len(label)):
+        pred = int(label[i])
+        tg = int(target[i])
+        matrix[pred][tg]+=1
+    return matrix
+
+def evaluate_and_save(matrix):
+    df = pd.DataFrame(matrix)
+    df['index'] = CHARS[:-1]
+    df.set_index('index',drop=True,inplace=True)
+    df.to_csv("confusion_matrix_traindata.csv",header=CHARS[:-1])
+    y_true = []
+    y_pred = []
+    for i in range(len(matrix)):
+        for j in range(len(matrix[0])):
+            for _ in range(int(matrix[i][j])):
+                y_true.append(j)
+                y_pred.append(i)
+    report = classification_report(y_true, y_pred, target_names=CHARS[:-1], output_dict=True)
+    pd.DataFrame(report).transpose().to_csv('Cls_report_traindata.csv')
+
+
 
 def show(img, label, target):
     img = np.transpose(img, (1, 2, 0))
